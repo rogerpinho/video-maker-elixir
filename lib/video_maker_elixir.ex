89d987a853640be4ac3979b0cee029cc6290190e
Roger Vezaro <rogervezaro1997@gmail.com>
2019-02-25 22:15:14 -0400

    feat: add search term and prefix

--- lib/video_maker_elixir.ex
+++ lib/video_maker_elixir.ex
@@ -1,10 +1,19 @@
 defmodule VideoMakerElixir.CLI do
 
   def main(_args) do
-    say_hello()
+    searchTerm = askAndReturnSearchTerm()
+    prefix = askAndReturnPrefix()
   end
 
-  defp say_hello() do
-    IO.puts("Hello, world!")
+  def askAndReturnSearchTerm() do
+    ExPrompt.get("Type a Wikipedia search term: ")
   end
+
+  def askAndReturnPrefix() do
+    prefixes = ["Who is", "What is", "The history of"]
+    selectedPrefixIndex = ExPrompt.choose("Choose your option!", prefixes)    
+    
+    selectedPrefixText = Enum.at(prefixes, selectedPrefixIndex)
+  end
+
 end
